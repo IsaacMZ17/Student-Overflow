50f3167d5ced647d45549b8d725b5e768b094ae8
IsaacMZ17 <moraleseduardoiz050817@gmail.com>
2024-08-25 07:58:20 -0600

    Configuración final para Heroku

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import crear_app
 from flask_migrate import Migrate
 from flask.cli import FlaskGroup
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 app = crear_app()
 migrate = Migrate(app, app.extensions['migrate'].db)
