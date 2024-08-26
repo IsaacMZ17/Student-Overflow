486b3bc606132da0158440958645745988bb5792
IsaacMZ17 <moraleseduardoiz050817@gmail.com>
2024-08-25 18:26:45 -0600

    NHK

--- run.py
+++ run.py
@@ -2,9 +2,6 @@ from app import crear_app
 from flask_migrate import Migrate
 from flask.cli import FlaskGroup
 from dotenv import load_dotenv
-import os
-
-load_dotenv() 
 
 app = crear_app()
 migrate = Migrate(app, app.extensions['migrate'].db)
@@ -13,4 +10,3 @@ cli = FlaskGroup(app)
 
 if __name__ == '__main__':
     cli()
-    app.run(host='127.0.0.1', port=int(os.environ.get("PORT", 5000)))
